e3def371562ebec6612666c82fa6284b39ecb8be
RinEster <kliuevairina05@gmail.com>
2025-12-10 21:23:30 +0400

    feat: add more mixin

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -14,25 +14,29 @@ from .models import Birthday
 
 class BirthdayMixin:
     model = Birthday
-    form_class = BirthdayForm
-    template_name = 'birthday/birthday.html'
     success_url = reverse_lazy('birthday:list')
 
-class BirthdayUpdateView(BirthdayMixin, UpdateView):
+
+class BirthdayFormMixin:
+    form_class = BirthdayForm
+    template_name = 'birthday/birthday.html'
+
+
+class BirthdayCreateView(BirthdayMixin, BirthdayFormMixin, CreateView):
     pass
 
-class BirthdayCreateView(BirthdayMixin, CreateView):
+
+class BirthdayUpdateView(BirthdayMixin, BirthdayFormMixin, UpdateView):
     pass
+
+
+class BirthdayDeleteView(BirthdayMixin, DeleteView):
+    pass 
 
 class BirthdayListView(ListView):
     model = Birthday
     ordering = 'id'
     paginate_by = 10
-
-class BirthdayDeleteView(DeleteView):
-    model = Birthday
-    template_name = 'birthday/birthday.html'
-    success_url = reverse_lazy('birthday:list') 
 
 def birthday(request, pk=None):
     if pk is not None:
