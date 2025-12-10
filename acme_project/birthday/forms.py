54615a6e620a3b2a78652688eb5ebf205b9fce7c
RinEster <kliuevairina05@gmail.com>
2025-12-11 00:41:17 +0400

    feat: add congratulations

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Birthday 
+from .models import Birthday, Congratulation
 
 from django.core.mail import send_mail
 
@@ -32,3 +32,9 @@ class BirthdayForm(forms.ModelForm):
             raise ValidationError(
                 'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
             ) 
+
+class CongratulationForm(forms.ModelForm):
+    
+    class Meta:
+        model = Congratulation
+        fields = ('text',) 
