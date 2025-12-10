2b37c2f12cf8d795973cf8f77fdb68cc6bf0412a
RinEster <kliuevairina05@gmail.com>
2025-12-10 22:40:10 +0400

    feat: add info index.html

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,6 +1,5 @@
-# birthday/models.py
 from django.db import models
-
+from django.urls import reverse
 
 class Birthday(models.Model):
     first_name = models.CharField('Имя', max_length=20)
@@ -9,3 +8,6 @@ class Birthday(models.Model):
     )
     birthday = models.DateField('Дата рождения')
     image = models.ImageField('Фото', upload_to='birthdays_images', blank=True)
+
+    def get_absolute_url(self):
+        return reverse('birthday:detail', kwargs={'pk': self.pk}) 
